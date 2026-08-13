666457dcdbb81791b199c0f38ae977184f036173
Elirus <thedeveloperpauksa@gmail.com>
2026-08-14 02:22:44 +0630

    using dynamic classnames, show active tab at parent, then props to another

--- src/app/components/tab/Tab.jsx
+++ src/app/components/tab/Tab.jsx
@@ -1,17 +1,29 @@
 
 import styles from "./Tab.module.css";
 import {useState} from "react";
+import classNames from "classnames";
+
+function TabHeader({title, onClick, active}) {
+    console.log('tabHeader ', styles['tab-header']);
+    const tabClass = classNames({
+        [styles['tab-header']]: true,
+        [styles['active-tab']]: active,
+    });
+    return <div className={tabClass}
+                onClick={onClick}>
+        {title}
+    </div>;
+}
 
 export default function Tab({titles, children})
 {
     const [activeTab, setActiveTab] = useState(0);
     return (<div>
         {
-            titles.map((title, index) => <div className={styles['tab-header']}
-                                              key={index}
-                                            onClick = {() => setActiveTab(index)}>
-                {title}
-            </div>)
+            titles.map((title, index) => <TabHeader key={index}
+                                                    onClick={() => setActiveTab(index)}
+                                                    title={title}
+            active={activeTab === index}/>)
         }
         <div className={styles['tab-content']}>
             {
