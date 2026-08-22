611bb7f4253528577d70da9309e231b1e4bdc6e2
Elirus <thedeveloperpauksa@gmail.com>
2026-08-23 00:09:26 +0630

    todo reducer func, collectting statemanagement all together

---
+++ src/app/components/reducer/TodoReducer.jsx
@@ -0,0 +1,10 @@
+export function todoReducer(state, action) {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, action.payload]
+        case 'UPDATE_TODO':
+            return state.map(td => td.id === action.payload.id ? action.payload : td);
+        case 'DELETE_TODO':
+            return state.filter(td => td.id !== action.payload.id)
+    }
+}
